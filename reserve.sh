ef13a7bf7d1dfeb392772442eecdb261c5e79d4c
James Chen <niche@vt.edu>
2025-04-18 14:32:40 -0400

    revision

---
+++ reserve.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=InTeRaCtIvE
+#SBATCH -t 143:59:59
+#SBATCH --partition=a100_normal_q
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --account=niche_squad
+#SBATCH --output=InTeRaCtIvE.out
+
+while true
+do
+  echo "This loop will run forever..."
+  sleep 10
+done
